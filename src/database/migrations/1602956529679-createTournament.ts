1137647a83d9e40cc7dda8fe717a348d62a1ac6a
João Soares <jpcirqueira81@gmail.com>
2020-10-17 17:01:45 -0300

    [EP-03] Adicionado migrate de torneio.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- src/database/migrations/1602956529679-createTournament.ts
+++ src/database/migrations/1602956529679-createTournament.ts
@@ -1,55 +1,73 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class createTournament1602956529679 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(
+      new Table({
+        name: 'tournaments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'text',
+          },
+          {
+            name: 'type',
+            type: 'char',
+          },
+          {
+            name: 'rules',
+            type: 'text',
+          },
+          {
+            name: 'players_quantity',
+            type: 'integer',
+          },
+          {
+            name: 'start_date',
+            type: 'date',
+          },
+          {
+            name: 'end_date',
+            type: 'date',
+          },
+          {
+            name: 'estado',
+            type: 'varchar',
+          },
+          {
+            name: 'cidade',
+            type: 'varchar',
+          },
+          {
+            name: 'endereco',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+          },
+        ],
+      }),
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-        await queryRunner.createTable(
-            new Table({
-                name: "tournaments",
-                columns:[{
-                    name: 'id',
-                    type: 'varchar',
-                    isPrimary: true,
-                    generationStrategy: 'uuid',
-                    default:'uuid_generate_v4()'
-                },
-                {
-                    name: 'name',
-                    type: 'varchar',
-                },
-                {
-                    name: 'description',
-                    type: 'varchar',
-                },
-                {
-                    name: 'type',
-                    type: 'char',
-                },
-                {
-                    name: 'rules',
-                    type: 'varchar',
-                },
-                {
-                    name: 'players_quantity',
-                    type: 'integer'
-                },
-                {
-                    name: 'manager_id',
-                    type: 'varchar[]'
-                },
-                {
-                    name: 'location',
-                    type: 'varchar'
-                }
-            ]
-            })
-
-        );
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('tournaments')
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tournaments')
+  }
 }
