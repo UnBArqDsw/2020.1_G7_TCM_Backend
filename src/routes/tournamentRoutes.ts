6086cb06fda6561db2fc50e8ca770bd2ffcb9ca8
João Soares <jpcirqueira81@gmail.com>
2020-10-17 17:57:29 -0300

    [EP03] Adicionado controller tournament.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- src/routes/tournamentRoutes.ts
+++ src/routes/tournamentRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import tournamentController from '../controllers/tournament/index'
 
 const tournamentRoutes = Router()
 
 tournamentRoutes.get('/tournament', (request, response) => {
   response.json({ message: 'tournaments' })
+})
+
+tournamentRoutes.post('/tournament', (request, response) => {
+  tournamentController.createTournament(request, response)
 })
 
 export default tournamentRoutes
