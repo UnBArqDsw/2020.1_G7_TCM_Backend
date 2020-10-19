4e3a3ae7f40c66ff5046534b186a1b3cd9024256
lucas <lucas.alexandre.df@gmail.com>
2020-10-19 16:58:11 -0300

    Criado check para senha da sessao
    Signed-off-by: lucas <lucas.alexandre.df@gmail.com>
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>

--- src/services/session/createSessionService.ts
+++ src/services/session/createSessionService.ts
@@ -1,6 +1,5 @@
-import { response, Response } from 'express'
-
 import { getRepository } from 'typeorm'
+import { compare } from 'bcryptjs'
 import AppError from '../../errors/appError'
 import User from '../../models/user/user'
 
@@ -13,10 +12,15 @@ class CreateSessionService {
   public async execute(email: string, password: string): Promise<Session> {
     const userRepository = getRepository(User)
     const user = await userRepository.findOne({ where: { email } })
-
     if (!user) {
       throw new AppError('Email ou senha inválidos', 401)
     }
+    const checkPassword = await compare(password, user.password)
+
+    if (!checkPassword) {
+      throw new AppError('Email ou senha inválidos', 401)
+    }
+
     return { email }
   }
 }
