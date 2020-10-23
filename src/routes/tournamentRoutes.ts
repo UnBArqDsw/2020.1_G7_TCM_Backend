47fcc8dd30575ac9479ad6e4daa75aa16965e4a0
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-10-23 18:34:58 -0300

    Adicionada Rota para pesquisa de torneio

--- src/routes/tournamentRoutes.ts
+++ src/routes/tournamentRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { CreateTournamentController } from '../controllers/tournament/createTournamentController'
+import { SearchTournamentController } from '../controllers/tournament/searchTournamentController'
 import userAuth from '../middlewares/userAuth'
 
 const tournamentRoutes = Router()
 
-tournamentRoutes.get('/tournament', userAuth, (request, response) => {
-  response.json({ message: 'tournaments' })
+tournamentRoutes.get('/tournament', userAuth, async (request, response) => {
+  const tournament = new SearchTournamentController()
+  await tournament.handle(request, response)
 })
 
 tournamentRoutes.post('/tournament', userAuth, async (request, response) => {
