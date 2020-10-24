35f30aef59fb47266662fb468e63cc2c58c52588
João Soares <jpcirqueira81@gmail.com>
2020-10-24 16:49:35 -0300

    [FT04] Adicionado service para criar rounds.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

---
+++ src/services/round/createRoundService.ts
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-restricted-syntax */
+import { Request } from 'express'
+import { getRepository } from 'typeorm'
+import { Result, Service } from '../protocols/IServices'
+import AppError from '../../errors/appError'
+import Round from '../../models/round/round'
+
+export class CreateRound implements Service {
+  public async execute(request: Request): Promise<Result> {
+    const { name, status } = request.body
+
+    const requiredFields = ['name', 'status']
+
+    for (const fields of requiredFields) {
+      if (!request.body[fields]) {
+        throw new AppError(`Missing Param: ${fields}`)
+      }
+    }
+
+    const roundRepository = getRepository(Round)
+
+    const round = roundRepository.create({
+      name,
+      status,
+    })
+
+    await roundRepository.save(round)
+
+    return { body: { round }, statusCode: 200 }
+  }
+}
