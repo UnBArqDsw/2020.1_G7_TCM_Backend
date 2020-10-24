b051073aeaad0d68666fe23ffaae13508c15edec
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-10-24 18:16:44 -0300

    Criada controller updateParticipantController.
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: Renan Cristyan <rcristyan9@gmail.com>

--- src/routes/participantRoute.ts
+++ src/routes/participantRoute.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express'
 import CreateParticipantController from '../controllers/participant/createParticipantController'
 import SearchParticipantController from '../controllers/participant/searchParticipantController'
+import UpdateParticipantController from '../controllers/participant/updateParticipantController'
 import userAuth from '../middlewares/userAuth'
 
 const participantController = Router()
-
-// participantController.get('/participant', userAuth, (request, response) => {
-//   response.json({ message: 'participant' })
-// })
 
 participantController.get('/participant/:id', async (request, response) => {
   const searchParticipant = new SearchParticipantController()
@@ -17,6 +14,11 @@ participantController.get('/participant/:id', async (request, response) => {
 participantController.post('/participant', async (request, response) => {
   const createParticipant = new CreateParticipantController()
   await createParticipant.handle(request, response)
+})
+
+participantController.post('/participant/:id', async (request, response) => {
+  const updateParticipant = new UpdateParticipantController()
+  await updateParticipant.handle(request, response)
 })
 
 export default participantController
