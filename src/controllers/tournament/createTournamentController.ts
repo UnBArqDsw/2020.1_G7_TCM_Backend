4e6d004f7774a19d487a3fc00fa398176a3d699f
João Soares <jpcirqueira81@gmail.com>
2020-10-25 21:21:10 -0300

    [E02] Adicionado logica ao factory torneio.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>

--- src/controllers/tournament/createTournamentController.ts
+++ src/controllers/tournament/createTournamentController.ts
@@ -1,10 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express'
-import { CreateTournamentFactory } from '../../services/factories/createTournamentsFactory'
+import { CreateTournamentFactoryPlayoff } from '../../services/factories/createTournamentsFactory'
 import { Controller } from '../protocols/IController'
 
 export class CreateTournamentController implements Controller {
   async handle(request: Request, response: Response): Promise<Response> {
-    const create = new CreateTournamentFactory()
+    let create: any
+    if (request.body.type === 'P') {
+      create = new CreateTournamentFactoryPlayoff()
+    }
+
     const { body, statusCode } = await create.execute(request)
     return response.status(statusCode).json(body)
   }
--- src/services/factories/createTournamentsFactory.ts
+++ src/services/factories/createTournamentsFactory.ts
@@ -2,7 +2,7 @@ import { ServiceCreator } from '../creator/serviceCreator'
 import { Service } from '../protocols/IServices'
 import { CreatePlayoff } from '../tournaments/createPlayoff'
 
-export class CreateTournamentFactory extends ServiceCreator {
+export class CreateTournamentFactoryPlayoff extends ServiceCreator {
   public factoryMethod(): Service {
     return new CreatePlayoff()
   }
