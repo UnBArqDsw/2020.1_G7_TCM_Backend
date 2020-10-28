a7ea5fcd33e8a6b0683c351c040a63be93875f79
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-10-28 19:19:22 -0300

    Adicionado model de partida

---
+++ src/models/match/match.ts
@@ -0,0 +1,55 @@
+/* eslint-disable camelcase */
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm'
+
+import User from '../user/user'
+import Participants from '../participant/participant'
+
+@Entity('matchs')
+class Matchs {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  status: string
+
+  @Column()
+  player1: string
+
+  @Column()
+  player2: string
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'player1' })
+  player1_id: User
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'player2' })
+  player2_id: User
+
+  @OneToOne(() => Participants)
+  @JoinColumn({ name: 'player1' })
+  participant1_id: Participants
+
+  @OneToOne(() => Participants)
+  @JoinColumn({ name: 'player2' })
+  participant2_id: Participants
+
+  @Column()
+  score: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
+
+export default Matchs
