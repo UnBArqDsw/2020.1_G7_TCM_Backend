5d84b065519dd0bef8cb4f73a067f51519b169d1
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-11-01 03:16:58 -0300

    Adicionada rota da partida.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import userRoutes from './userRoutes'
 import tournamentRoutes from './tournamentRoutes'
 import sessionRoutes from './sessionRoutes'
 import participantRoutes from './participantRoute'
+import matchRoutes from './matchRoutes'
 
 const routes = Router()
 
@@ -11,5 +12,6 @@ routes.use(userRoutes)
 routes.use(tournamentRoutes)
 routes.use(sessionRoutes)
 routes.use(participantRoutes)
+routes.use(matchRoutes)
 
 export default routes
