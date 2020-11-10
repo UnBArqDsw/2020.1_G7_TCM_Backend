7dde23144c1106875e7d6b74262302a41c69ab96
lucas <lucas.alexandre.df@gmail.com>
2020-11-09 23:48:06 -0300

    Modificada a service de createRound
    Signed-off-by: lucas <lucas.alexandre.df@gmail.com>
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: João Pedro <jpcirqueira81@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- src/services/round/createRoundService.ts
+++ src/services/round/createRoundService.ts
@@ -3,17 +3,29 @@
 import { getRepository } from 'typeorm'
 import { Result } from '../protocols/IServices'
 import Round from '../../models/round/round'
+import Matchs from '../../models/match/match'
 
 export class CreateRoundService {
   public async execute(name: string, status: boolean): Promise<Result> {
     const roundRepository = getRepository(Round)
+    const matchRepository = await getRepository(Matchs)
+    let match = new Matchs()
 
-    const round = roundRepository.create({
-      name,
-      status,
-    })
+    match = await matchRepository.findOne(
+      '79014fc8-2dd5-491a-ba78-8cae12c5bef2',
+    )
+    try {
+      let round = new Round()
+      round.name = name
+      round.status = status
+      round.matchs_ids = [match.id,match.id]
+      const round_aux = await roundRepository.save(round)
 
-    await roundRepository.save(round)
+    } catch (error) {
+      console.log(error)
+    }
+
+    // match.round_id =
 
     return { body: { round }, statusCode: 200 }
   }
