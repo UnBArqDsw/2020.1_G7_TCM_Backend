0258808dbd311dab71e51826e0c69d57f7860ad1
João Soares <jpcirqueira81@gmail.com>
2020-11-10 19:54:38 -0300

    Ajustado migrate de addcolumnroundandparticipants
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- src/database/migrations/1605048139775-addcolumnroundandparticipants.ts
+++ src/database/migrations/1605048139775-addcolumnroundandparticipants.ts
@@ -1,11 +1,26 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
 
-export class addcolumnroundandparticipants1605048139775 implements MigrationInterface {
+export class addcolumnroundandparticipants1605048139775
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'tournaments',
+      new TableColumn({
+        name: 'participants',
+        type: 'varchar',
+      }),
+    )
+    await queryRunner.addColumn(
+      'tournaments',
+      new TableColumn({
+        name: 'rounds_ids',
+        type: 'varchar',
+      }),
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tournaments')
+    await queryRunner.dropTable('rounds_ids')
+  }
 }
