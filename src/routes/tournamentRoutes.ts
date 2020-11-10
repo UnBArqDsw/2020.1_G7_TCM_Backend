33a6e1d350594f26b092664aff47ab52e7821cb4
RCristyan <rcristyan9@gmail.com>
2020-11-09 21:35:53 -0300

    Adicionada rota de solicitaçao em tournament.
    Signed-off-by: RCristyan <rcristyan9@gmail.com>
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>

--- src/routes/tournamentRoutes.ts
+++ src/routes/tournamentRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { CreateSolitiationController } from '../controllers/solicitation/createSolicititation'
 import { CreateTournamentController } from '../controllers/tournament/createTournamentController'
 import { SearchTournamentController } from '../controllers/tournament/searchTournamentController'
 import userAuth from '../middlewares/userAuth'
@@ -10,9 +11,18 @@ tournamentRoutes.get('/tournament', userAuth, async (request, response) => {
   await tournament.handle(request, response)
 })
 
-tournamentRoutes.post('/tournament', async (request, response) => {
+tournamentRoutes.post('/tournament', userAuth, async (request, response) => {
   const createTournament = new CreateTournamentController()
   await createTournament.handle(request, response)
 })
+
+tournamentRoutes.post(
+  '/solicitation/:tournamentId',
+  userAuth,
+  async (request, response) => {
+    const createSolicitation = new CreateSolitiationController()
+    await createSolicitation.handle(request, response)
+  },
+)
 
 export default tournamentRoutes
