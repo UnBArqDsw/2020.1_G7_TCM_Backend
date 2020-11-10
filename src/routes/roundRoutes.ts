7909ed9e585e871ae98c12540d5d959bd38909a3
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-11-10 08:34:35 -0300

    Ajustes para round.

--- src/routes/roundRoutes.ts
+++ src/routes/roundRoutes.ts
@@ -1,15 +1,23 @@
 import { Router, Request, Response } from 'express'
 import { CreateRoundController } from '../controllers/round/createRoundController'
+import { ListMatchsRoundController } from '../controllers/round/listMatchsRoundController'
 import userAuth from '../middlewares/userAuth'
 
 const roundRoutes = Router()
 
 roundRoutes.post(
   '/round',
-  userAuth,
   async (request: Request, response: Response) => {
     const CreateRound = new CreateRoundController()
     await CreateRound.handle(request, response)
+  },
+)
+
+roundRoutes.get(
+  '/round/:id',
+  async (request: Request, response: Response) => {
+    const list = new ListMatchsRoundController()
+    await list.handle(request, response)
   },
 )
 
--- src/services/round/createRoundService.ts
+++ src/services/round/createRoundService.ts
@@ -7,36 +7,37 @@ import Matchs from '../../models/match/match'
 import CreateMatchService from '../match/createMatchService'
 
 export class CreateRoundService {
-  public async execute(name: string, status: boolean): Promise<Result> {
+  public async execute(name: string, status: boolean, participant_ids: string[]): Promise<Result> {
     const roundRepository = getRepository(Round)
     const matchRepository = await getRepository(Matchs)
     let match = new Matchs()
 
-    let entrada = ['fb0c6ff6-1ed0-4fe0-9c50-63e864b32139','880b5c3d-98fa-4acb-bd3c-adad06d55f66','608b815f-18e0-4a83-86c6-fad55578ab06']
+    console.log("##############")
+      console.log(participant_ids)
+
     let matchs_ids:string[] = []
 
-    
-
     function participantRandom(){
-      let aux = Math.floor(Math.random() * entrada.length);
-      const participant = entrada[aux]
-      entrada = entrada.filter(v => v !== participant);
+      let aux = Math.floor(Math.random() * participant_ids.length);
+      const participant = participant_ids[aux]
+      participant_ids = participant_ids.filter(v => v !== participant);
       return participant;
     }
     const createMatch = new CreateMatchService()
 
-    if(entrada.length%2 ==0){
+    //verifica se o numero de participantes é impar
+    if(participant_ids.length%2 !==0){
       const particpant1 = participantRandom();
       const match_result = await createMatch.execute('',particpant1,'')
       matchs_ids.push(String(match_result.match_id))
 
     }
 
-    while(entrada.length != 0){
+    while(participant_ids.length != 0){
       const particpant1 = participantRandom();
       const particpant2 = participantRandom();
       
-      console.log("entrada", entrada);
+      // console.log("participant_ids", participant_ids);
       const match_result = await createMatch.execute('',particpant1,particpant2)
       matchs_ids.push(String(match_result.match_id))
     }
