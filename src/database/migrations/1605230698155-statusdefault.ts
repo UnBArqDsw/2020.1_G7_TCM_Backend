2e6cbc919eec2db7a869d21f1babef7eb6a5241d
lucas <lucas.alexandre.df@gmail.com>
2020-11-13 00:34:32 -0300

    Ajustes no eslint

--- src/database/migrations/1605230698155-statusdefault.ts
+++ src/database/migrations/1605230698155-statusdefault.ts
@@ -1,14 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class statusdefault1605230698155 implements MigrationInterface {
-    name = 'statusdefault1605230698155'
+  name = 'statusdefault1605230698155'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tournaments" ALTER COLUMN "status" SET DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "tournaments" ALTER COLUMN "status" SET DEFAULT false`,
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tournaments" ALTER COLUMN "status" DROP DEFAULT`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "tournaments" ALTER COLUMN "status" DROP DEFAULT`,
+    )
+  }
 }
