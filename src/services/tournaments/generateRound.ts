efd27662273e5f089d547a6e7395daa9deaade2b
lucas <lucas.alexandre.df@gmail.com>
2020-11-12 22:30:34 -0300

    Ajustado as services de torneio
    Signed-off-by: lucas <lucas.alexandre.df@gmail.com>
    Co-authored-by: João Pedro <jpcirqueira81@gmail.com>

--- src/services/tournaments/generateRound.ts
+++ src/services/tournaments/generateRound.ts
@@ -3,5 +3,10 @@ import { Request } from 'express'
 import { Result, Service } from '../protocols/IServices'
 
 export class GenerationRound implements Service {
-  public async execute(request: Request): Promise<Result> {}
+  public async execute(request: Request): Promise<Result> {
+    const tournament = request.params
+    const { players } = request.body
+
+    return { body: { tournament, players }, statusCode: 200 }
+  }
 }
