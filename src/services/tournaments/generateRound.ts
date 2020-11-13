76d476b37ec03899f5ca5aa3998795a1df137b97
João Soares <jpcirqueira81@gmail.com>
2020-11-13 00:27:46 -0300

    Adicionado logica para serviço de gerar round.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- src/services/tournaments/generateRound.ts
+++ src/services/tournaments/generateRound.ts
@@ -1,12 +1,38 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-console */
 /* eslint-disable no-restricted-syntax */
 import { Request } from 'express'
+import { getRepository } from 'typeorm'
 import { Result, Service } from '../protocols/IServices'
+import Tournaments from '../../models/tournament/tournament'
+import { CreateRoundService } from '../round/createRoundService'
 
 export class GenerationRound implements Service {
   public async execute(request: Request): Promise<Result> {
-    const tournament = request.params
-    const { players } = request.body
+    const id = request.params
+    const name = request.body
+    const tournamentRepository = getRepository(Tournaments)
+    const tournament = await tournamentRepository.findOne({
+      where: id,
+    })
+    const players = tournament.participants
 
-    return { body: { tournament, players }, statusCode: 200 }
+    const player_id = []
+    for (const player in players) {
+      // console.log()
+      player_id.push(players[player].id)
+    }
+    // console.log(player_id)
+    // console.log(ids)
+    // console.log(players)
+    // const players = [
+    //   '642b013f-da49-4d36-b0a2-c9dfb554595a',
+    //   '96e7a0a4-16e4-4cdd-8695-abccf8b026aa',
+    // ]
+    // const num_players = players.length
+    const createRound = new CreateRoundService()
+
+    const round = await createRound.execute(name, true, player_id)
+    return { body: { round, players }, statusCode: 200 }
   }
 }
