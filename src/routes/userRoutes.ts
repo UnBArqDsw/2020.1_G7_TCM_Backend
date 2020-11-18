68a2e4da7fed5a1141cbebe9a118dde363906781
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-11-18 13:35:33 -0300

    Adicionada rota para controller searchMyParticipartionController.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express'
 
 import { CreateUserController } from '../controllers/user/createUserController'
 import { SearchUserController } from '../controllers/user/searchUserController'
+import { searchMyParticipartionController } from '../controllers/user/searchMyParticipartionController'
+import userAuth from '../middlewares/userAuth'
 
 const userRoutes = Router()
 
@@ -9,10 +11,19 @@ userRoutes.post('/user', async (request: Request, response: Response) => {
   const createUser = new CreateUserController()
   await createUser.handle(request, response)
 })
+// userRoutes.get(
+//   '/user/:nickname',
+//   async (request: Request, response: Response) => {
+//     console.log("chegouuuuuu")
+//     const SearchUser = new SearchUserController()
+//     await SearchUser.handle(request, response)
+//   },
+// )
+
 userRoutes.get(
-  '/user/:nickname',
+  '/user/tournaments',userAuth,
   async (request: Request, response: Response) => {
-    const SearchUser = new SearchUserController()
+    const SearchUser = new searchMyParticipartionController()
     await SearchUser.handle(request, response)
   },
 )
